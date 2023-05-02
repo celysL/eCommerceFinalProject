3112ef8a94a6961589e151421b9eefab5d85bed0
Marc-Eric Boury <meboury@hotmail.com>
2023-05-01 20:28:20 -0400

    créé des fonctions globales pour obtenir des connections PDO ou mysqli. Ajouté un autoloader pour les classes.

--- eCommerceFinalProject/include/config.php
+++ eCommerceFinalProject/include/config.php
@@ -15,6 +15,11 @@ $db_user = 'root';
 $db_pass = '';
 $db_name = 'commerce_final_project';
 
+
+//mysqli connection
+$conn = mysqli_connect('localhost', 'root', '', 'commerce_final_project') or die('connection failed');
+$conn->query("SET @@auto_increment_increment=1");
+
 //PDO connection
 try{
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
@@ -24,9 +29,46 @@ catch(PDOException $exception){
     echo "PDO connection failed: " . $exception->getMessage();
 }
 
+function getPdoConnection() : PDO {
+    static $pdo;
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_pass = '';
+    $db_name = 'commerce_final_project';
+    if (!($pdo instanceof PDO)) {
+        try{
+            $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $exception){
+            echo "PDO connection failed: " . $exception->getMessage();
+        }
+    }
+    return $pdo;
+    
+}
+
+function getMysqliConnection() : mysqli {
+    static $mysqli;
+    if (!($mysqli instanceof mysqli)) {
+        $mysqli = mysqli_connect('localhost', 'root', '', 'commerce_final_project') or die('connection failed');
+        $mysqli->query("SET @@auto_increment_increment=1");
+    }
+    return $mysqli;
+}
 
 
-//mysqli connection
-$conn = mysqli_connect('localhost', 'root', '', 'commerce_final_project') or die('connection failed');
-$conn->query("SET @@auto_increment_increment=1");
+// CLASS AUTOLOADER
+$psr4_autoloader = function(string $class) : bool {
+    $file = str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
+    $file_path = __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."classes".DIRECTORY_SEPARATOR.$file;
+    if (file_exists($file_path)) {
+        require_once $file_path;
+        return true;
+    }
+    return false;
+};
+
+spl_autoload_register($psr4_autoloader);
+
 ?>
