213cb934f7eb8c9dae990f048929b1ea2ccb8ca1
celysL <yingshanliang28@gmail.com>
2023-05-03 00:38:51 -0400

    css added and site is pretty

--- eCommerceFinalProject/index.php
+++ eCommerceFinalProject/index.php
@@ -24,7 +24,8 @@ $products = getProducts();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product List</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
+    
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function() {
